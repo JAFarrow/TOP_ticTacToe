40ba7d46b0e5bca4ae07e474d2bef7787cce3557
JAFarrow <f.justin@tutanota.com>
2023-06-07 21:33:16 +0200

    Implemented Unbeatable AI Unbeatable AI introduced using minMax Algo After a fair amount of hairs pulled out, an unbeatable option has been introduced into the game logic. Currently only able to be invoked in code and not the UI yet.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const domManipulation = (function() {
         startButton.addEventListener('click', () => {
             boardInstantiation();
             if (gameState.getPlayerToken() != 'X') {
-                gameLogic.gameFlow('computer', gameLogic.easyAI());
+                gameLogic.gameFlow('computer', gameLogic.impossibleAI());
             };
             startButton.disabled = true;
         });
@@ -27,7 +27,7 @@ const domManipulation = (function() {
             gameState.resetState();
             boardInstantiation();
             if (gameState.getPlayerToken() === 'O') {
-                gameLogic.gameFlow('computer', gameLogic.easyAI());
+                gameLogic.gameFlow('computer', gameLogic.impossibleAI());
             }
         });
 
@@ -54,10 +54,6 @@ const domManipulation = (function() {
                 gameState.tileArraySetter(gameTile.id);
                 gameContainer.style.backgroundColor = '#000';
                 gameTile.addEventListener('click', () => {
-                    gameTile.classList.add(
-                        gameState.getPlayerToken() === 'X' ?
-                         'gameTileCross' : 'gameTileNought'
-                    );
                     gameLogic.gameFlow('player', gameTile.id);
                 });
                 gameContainer.appendChild(gameTile);
@@ -88,7 +84,11 @@ const agentFactory = (name) => {
         _agentMoves = [];
     };
 
-    return { name, getMoves, pushMove, resetArray };
+    const cloneAgentMoves = function() {
+        return [..._agentMoves];
+    }
+
+    return { name, getMoves, pushMove, resetArray, cloneAgentMoves };
 };
 
 const gameState = (function() {
@@ -96,8 +96,8 @@ const gameState = (function() {
 
     let _playerToken = '';
 
-    const _humanPlayer = agentFactory('Player', "X");
-    const _computerPlayer = agentFactory('Computer', "O")
+    const _humanPlayer = agentFactory('Player');
+    const _computerPlayer = agentFactory('Computer')
 
     let _tileArray = [];
 
@@ -123,8 +123,12 @@ const gameState = (function() {
 
     const tileArrayPop = function(selectedTile) {
         let tileIndex = _tileArray.indexOf(`${selectedTile}`)
-        _tileArray.splice(tileIndex, 1);
-    };;
+        _tileArray = _tileArray.toSpliced(tileIndex, 1);
+    };
+
+    const cloneTileArray = function() {
+        return [..._tileArray];
+    }
 
     const setPlayerToken = function(token) {
         _playerToken = token;
@@ -147,6 +151,7 @@ const gameState = (function() {
         tileArrayGetter,
         tileArrayClear,
         tileArrayPop,
+        cloneTileArray,
         setPlayerToken,
         getPlayerToken,
         resetState,
@@ -156,11 +161,26 @@ const gameState = (function() {
 const gameLogic = (function() {
     'use strict';
 
-    //Game logic used in event listener for each game tile.
     const tileInteractionLogic = function(agent, selectedTile) {
+        let selectedGameTile = document.getElementById(selectedTile);
+
         if (gameState.tileArrayGetter().indexOf(selectedTile) != -1) {
             gameState.tileArrayPop(selectedTile);
-            agent.pushMove(selectedTile);
+            if (agent === 'player') {
+                gameState.accessHumanPlayerObj().pushMove(selectedTile);
+            } else if (agent === 'computer') {
+                gameState.accessComputerPlayerObj().pushMove(selectedTile);
+            }
+            switch(gameState.getPlayerToken()) {
+                case 'O':
+                    selectedGameTile.classList.add(
+                    agent === 'player' ? 'gameTileNought' : 'gameTileCross' );
+                    break;
+                case 'X': 
+                    selectedGameTile.classList.add(
+                    agent === 'player' ? 'gameTileCross' : 'gameTileNought' );
+                    break;
+            }
         } else {
             console.log('Error!');
         }
@@ -169,18 +189,75 @@ const gameLogic = (function() {
     const easyAI = function() {
         let randomPick = Math.floor(Math.random() * gameState.tileArrayGetter().length);
         let pickIndex = gameState.tileArrayGetter()[randomPick];
-        let tileSelection = document.getElementById(pickIndex);
-        tileSelection.classList.add(
-            gameState.getPlayerToken() === 'X' ?
-            'gameTileNought' : 'gameTileCross'
-        );
         return pickIndex;
     };
 
-    const winCheck = function(array) {
-        const stringifyArray = (array) => {return array.join('')};
+    const impossibleAI = function() {
+        let realGameBoard = gameState.cloneTileArray();
+        let computerMoves = gameState.accessComputerPlayerObj().cloneAgentMoves();
+        let humanMoves = gameState.accessHumanPlayerObj().cloneAgentMoves();
 
-        const regExArr = [
+        const bestMove = function(gameBoard, currentComputerMoves, currentHumanMoves) {
+
+            let bestScore = -1000;
+            let bestMove = '';
+            for (let move = 0; move <= gameBoard.length - 1; move++) {
+                let newMove = gameBoard[move];
+                let gameBoardMinusMove = gameBoard.toSpliced(move, 1);
+                let computerMovesPlusNewMove = [...currentComputerMoves];
+                computerMovesPlusNewMove.push(newMove);
+                let newScore = miniMax(gameBoardMinusMove, computerMovesPlusNewMove, currentHumanMoves, gameBoardMinusMove.length, false);
+                if (newScore > bestScore) {
+                    bestScore = newScore;
+                    bestMove = newMove;
+                };
+                console.log(`Best Move = ${bestMove} & Best Score = ${bestScore}`);
+            };
+            return bestMove;
+        };
+
+        const miniMax = function(gameBoard, maxiMoves, miniMoves, depth, isMaximiser) {
+            if (winCheck(maxiMoves) === true) {
+                return 10 - depth;
+            };
+
+            if (winCheck(miniMoves) === true) {
+                return depth - 10;
+            };
+
+            if (depth === 0) {
+                return 0;
+            };
+
+
+            if (isMaximiser) {
+                let maxiBestScore = -1000;
+                for (let move = 0; move <= depth - 1; move++) {
+                    let gameBoardMinusMove = gameBoard.toSpliced(move, 1);
+                    let maxiArrayPlusMove = [...maxiMoves];
+                    maxiArrayPlusMove.push(gameBoard[move]);
+                    let miniMovesClone = [...miniMoves];
+                    maxiBestScore = Math.max(maxiBestScore, miniMax(gameBoardMinusMove, maxiArrayPlusMove, miniMovesClone, gameBoardMinusMove.length, false));
+                };
+                return maxiBestScore;
+            } else {
+                let miniBestScore = 1000;
+                for (let move = 0; move <= depth - 1; move++) {
+                    let gameBoardMinusMove = gameBoard.toSpliced(move, 1);
+                    let miniArrayPlusMove = [...miniMoves];
+                    miniArrayPlusMove.push(gameBoard[move]);
+                    let maxiMovesClone = [...maxiMoves];
+                    miniBestScore = Math.min(miniBestScore, miniMax(gameBoardMinusMove, maxiMovesClone, miniArrayPlusMove, gameBoardMinusMove.length, true));
+                };
+                return miniBestScore;
+            };
+        };
+        return bestMove(realGameBoard, computerMoves, humanMoves);
+
+    };
+
+    const regExInit = function() {
+        const regEx = [
             "(?=.*1a)(?=.*2a)(?=.*3a)",
             "(?=.*1b)(?=.*2b)(?=.*3b)",
             "(?=.*1c)(?=.*2c)(?=.*3c)",
@@ -189,61 +266,57 @@ const gameLogic = (function() {
             "(?=.*3a)(?=.*3b)(?=.*3c)",
             "(?=.*1a)(?=.*2b)(?=.*3c)",
             "(?=.*1c)(?=.*2b)(?=.*3a)"
-        ];
-        const regExTest = new RegExp(regExArr.join('|'));
+            ];
+            const regExTest = new RegExp(regEx.join('|'));
+            return regExTest;
+    }
 
-        switch (regExTest.test(stringifyArray(array))) {
-            case true:
-                return 'win';
-                break;
-            case false:
-                if (gameState.tileArrayGetter().length === 0) {
-                    return 'tie';
-                } else {
-                    return false;
-                }
-        };
+    const winRegExTest = regExInit();
+
+    const winCheck = function(array) {
+        const stringifyArray = (array) => {return array.join('')};
+        return (winRegExTest.test(stringifyArray(array)) === true ? true: false);
     };
 
     const gameFlow = function(agent, selectedTile) {
         switch(agent) {
             case 'player':
 
-                let player = gameState.accessHumanPlayerObj();
-                tileInteractionLogic(player, selectedTile);
+                tileInteractionLogic('player', selectedTile);
 
-                let playerWinTieCheck = winCheck(player.getMoves());
-                switch(playerWinTieCheck) {
+                switch(winCheck(gameState.accessHumanPlayerObj().getMoves())) {
                     case false:
-                        gameFlow('computer', easyAI());
+                        if (gameState.tileArrayGetter().length === 0) {
+                            console.log('Tie!');
+                        } else {
+                            gameFlow('computer', impossibleAI());
+                        }
                         break;
-                    case 'win':
-                        console.log(`${document.getElementById('nameInput').value} won!`);
-                        break;
-                    case 'tie':
-                        console.log('Tie!');
+                    case true:
+                        console.log(`${document.getElementById('nameInput').value} Win!`);
                 };
                 break;
 
             case 'computer':
 
-                let computer = gameState.accessComputerPlayerObj();
-                tileInteractionLogic(computer, selectedTile);
+                tileInteractionLogic('computer', selectedTile);
 
-                let computerWinTieCheck = winCheck(computer.getMoves());
-                switch(computerWinTieCheck) {
-                    case 'win':
+                switch(winCheck(gameState.accessComputerPlayerObj().getMoves())) {
+                    case true:
                         console.log('Computer Win!');
                         break;
-                    case 'tie':
-                        console.log('Tie!');
-                }
+                    case false:
+                        if (gameState.tileArrayGetter().length === 0) {
+                            console.log('Tie!')
+                        };
+                };
         };
     };
 
     return {
         gameFlow,
         easyAI,
+        impossibleAI,
     }
 })();
 
